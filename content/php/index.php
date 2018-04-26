b772f0bf2560acd2a831f3dd12106483a3f52a42
alnikulin <alnikyur@gmail.com>
2018-04-26 16:24:01 +0300

    Init project

---
+++ content/php/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Show user information like IP address, useragent
+**/
+$ip = $_SERVER['REMOTE_ADDR'];
+$browser = $_SERVER['HTTP_USER_AGENT'];
+$referrer = $_SERVER['HTTP_REFERER'];
+$host = $_SERVER['SERVER_NAME'];
+$hostname = gethostname();
+ if ($referred == "") {
+  $referrer = "This page was accessed directly";
+  }
+echo "<b>Visitor IP address:</b><br/>" . $ip . "<br/>";
+echo "<b>Browser (User Agent) Info:</b><br/>" . $browser . "<br/>";
+echo "<b>Referrer:</b><br/>" . $referrer . "<br/>";
+echo "<b>Host:</b><br/>" . $host . "<br/>";
+echo "<b>Hostname:</b><br/>" . $hostname . "<br/>";
+?>
